3c2f6a59bc807601cf82d7a4e62e9c8b86771ec2
Tom Beadie <>
2024-11-12 10:54:00 -0500

    Add more buttons, add listeners

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
+//--- Constants ----//
 
-console.log('calc')
+let num1 = document.getElementById("num1-el");
+let num2 = document.getElementById("num2-el");
+
+// Calc display
+let sumEl = document.getElementById("sum-el");
+// Math operator buttons
+const addBtn = document.getElementById("add-btn");
+const subBtn = document.getElementById("subtract-btn");
+const multBtn = document.getElementById("multiply-btn");
+const divBtn = document.getElementById("divide-btn");
+// Clear button
+const clearBtn = document.getElementById("clear-btn");
+
+//---- Functions ----//
+function add() {
+  addBtn.addEventListener("click", (e) => {
+    sumEl.innerHTML = num1 + num2;
+  });
+}
+function subtract() {
+  subBtn.addEventListener("click", (e) => {
+    sumEl.innerHTML = num1 - num2;
+  });
+}
+function add() {
+  multBtnBtn.addEventListener("click", (e) => {
+    sumEl.innerHTML = num1 * num2;
+  });
+}
+function add() {
+  divBtn.addEventListener("click", (e) => {
+    sumEl.innerHTML = num1 / num2;
+  });
+}
+// Clear display
+function clear(){
+    sumEl = ''
+}
