7debca709a0209895cadabff812e255b1ddbdf7c
Tom Beadie <>
2024-11-13 18:21:16 -0500

    CSS changes

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //--- Constants ----//
 
-let num1 = document.getElementById("num1-el");
-let num2 = document.getElementById("num2-el");
+
 
 // Calc display
-let sumEl = document.getElementById("sum-el");
+let display = document.getElementById("display");
 // Math operator buttons
 const addBtn = document.getElementById("add-btn");
 const subBtn = document.getElementById("subtract-btn");
@@ -15,26 +14,12 @@ const clearBtn = document.getElementById("clear-btn");
 
 //---- Functions ----//
 function add() {
-  addBtn.addEventListener("click", (e) => {
-    sumEl.innerHTML = num1 + num2;
+  addBtn.addEventListener("click", function () {
+    display = this.innerHTML;
   });
 }
-function subtract() {
-  subBtn.addEventListener("click", (e) => {
-    sumEl.innerHTML = num1 - num2;
-  });
-}
-function add() {
-  multBtnBtn.addEventListener("click", (e) => {
-    sumEl.innerHTML = num1 * num2;
-  });
-}
-function add() {
-  divBtn.addEventListener("click", (e) => {
-    sumEl.innerHTML = num1 / num2;
-  });
-}
+
 // Clear display
-function clear(){
-    sumEl = ''
+function clear() {
+  display = "";
 }
